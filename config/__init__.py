1d915b9b5577fcb2bbd174b405933d8bb6076c7c
jake Molly <74493337+2412322029@users.noreply.github.com>
2023-04-08 00:27:51 +0800

    azure 配置

--- config/__init__.py
+++ config/__init__.py
@@ -4,5 +4,10 @@ import yaml
 with open(os.path.join(os.path.dirname(__file__), '..', 'config.yaml'), 'r', encoding='utf8') as f:
     Config = yaml.safe_load(f)
 
+
 if __name__ == '__main__':
+    Config['databases']['host'] = os.environ['AZURE_MYSQL_HOST']
+    Config['databases']['username'] =os.environ['AZURE_MYSQL_USER']
+    Config['databases']['password'] =os.environ['AZURE_MYSQL_PASSWORD']
+    Config['databases']['dbname'] =os.environ['AZURE_MYSQL_NAME']
     print(Config)
